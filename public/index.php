5bc2ab40b9d09c3c5e96a57cac674031f5f8ccb6
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-10-18 22:04:24 -0400

    Setup cookie secret key so flash messages work.

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 $appDir = dirname(__DIR__);
 require_once "{$appDir}/vendor/autoload.php";
 
-$mailgunApiKey = getenv('MAILGUN_API_KEY') ?: die('Missing MAILGUN_API_KEY environment variable');
+$cookieSecretKey = getenv('COOKIE_SECRET_KEY') ?: die('Missing COOKIE_SECRET_KEY environment variable');
 
 $twigView = new \Slim\Views\Twig();
 $twigView->parserOptions = ['autoescape' => false];
 
-$app = new \Slim\Slim(['view' => $twigView, 'templates.path' => "{$appDir}/src/templates"]);
+$app = new \Slim\Slim([
+    'cookies.lifetime' => '1 month',
+    'cookies.secret_key' => $cookieSecretKey,
+    'view' => $twigView,
+    'templates.path' => "{$appDir}/src/templates"
+]);
+$app->add(new \Slim\Middleware\SessionCookie(['secret' => $cookieSecretKey, 'name' => 'session']));
 
 $view = $app->view();
 $view->parserExtensions = [new \Slim\Views\TwigExtension()];
