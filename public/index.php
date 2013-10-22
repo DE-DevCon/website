50a3866f609506f66f6dfbfa794407aca416001a
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-10-22 11:54:17 -0400

    Split out configuration.

--- public/index.php
+++ public/index.php
@@ -2,40 +2,10 @@
 $appDir = dirname(__DIR__);
 require_once "{$appDir}/vendor/autoload.php";
 
-$twigView = new \Slim\Views\Twig();
-$twigView->parserOptions = ['autoescape' => false];
+$app = new \Slim\Slim();
 
-$app = new \Slim\Slim(['view' => $twigView, 'templates.path' => "{$appDir}/src/templates"]);
-
-$app->container->singleton('mailgunDomain', function() {
-    $mailgunDomain = getenv('MAILGUN_DOMAIN');
-    if (!$mailgunDomain) {
-        throw new Exception('Missing MAILGUN_DOMAIN environment variable');
-    }
-
-    return $mailgunDomain;
-});
-
-$app->container->singleton('mailgun', function() {
-    $mailgunApiKey = getenv('MAILGUN_API_KEY');
-    if (!$mailgunApiKey) {
-        throw new Exception('Missing MAILGUN_API_KEY environment variable');
-    }
-
-    return new \Mailgun\Mailgun($mailgunApiKey);
-});
-
-$app->container->singleton('cfpEmail', function() {
-    $cfpEmail = getenv('CFP_EMAIL');
-    if (!$cfpEmail) {
-        throw new Exception('Missing CFP_EMAIL environment variable');
-    }
-
-    return $cfpEmail;
-});
-
-$view = $app->view();
-$view->parserExtensions = [new \Slim\Views\TwigExtension()];
+$configure = require "{$appDir}/src/config.php";
+$configure($app);
 
 $routes = require "{$appDir}/src/routes.php";
 $routes($app);
