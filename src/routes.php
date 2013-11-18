e600130ff063b27456e3d5fd1c1d0c3da365d54e
Chris Ryan <christopher.ryan@dominionenterprises.com>
2013-11-18 16:16:52 -0500

    Updated site to remove Call For Papers link and to add in the people topics schedule with cross links.

--- src/routes.php
+++ src/routes.php
@@ -4,30 +4,13 @@ return function(\Slim\Slim $app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
 
-    $app->get('/cfp', function() use($app) {
-        $app->render('cfp.html', ['page' => 'cfp']);
-    })->name('cfp');
+    $app->get('/topics', function() use($app) {
+        $app->render('topics.html', ['page' => 'topics']);
+    })->name('topics');
 
-    $app->post('/cfp', function() use($app) {
-        $req = $app->request();
-        try {
-            $app->mailgun->sendMessage(
-                $app->mailgunDomain,
-                [
-                    'from' => "{$req->post('name')} <{$req->post('email')}>",
-                    'to' => $app->cfpEmail,
-                    'subject' => 'Call For Papers',
-                    'text' => $req->post('summary'),
-                ]
-            );
-
-            $app->flashNow('success', 'Your proposal has been received.  Thanks for submitting!');
-        } catch (\Exception $e) {
-            $app->flashNow('error', 'There was an error saving your proposal.  Please verify your information and try again.');
-        }
-
-        $app->render('cfp.html', ['page' => 'cfp']);
-    });
+    $app->get('/people', function() use($app) {
+        $app->render('people.html', ['page' => 'people']);
+    })->name('people');
 
     $app->get('/faq', function() use($app) {
         $app->render('faq.html', ['page' => 'faq']);
