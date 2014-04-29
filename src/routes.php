6ba9bac5c4bc072ae39fbcdbc90c44ca359c51ad
Chris Ryan <christopher.ryan@dominionenterprises.com>
2014-04-29 14:39:46 -0400

    Changed site to early event information including adding the call for papers form back.

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,31 @@ return function(\Slim\Slim $app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
 
+    $app->get('/cfp', function() use($app) {
+        $app->render('cfp.html', ['page' => 'topics']);
+    })->name('cfp');
+
+    $app->post('/cfp', function() use($app) {
+        $req = $app->request();
+        try {
+            $app->mailgun->sendMessage(
+                $app->mailgunDomain,
+                [
+                    'from' => "{$req->post('name')} <{$req->post('email')}>",
+                    'to' => $app->cfpEmail,
+                    'subject' => 'Call For Papers',
+                    'text' => $req->post('summary'),
+                ]
+            );
+
+            $app->flashNow('success', 'Your proposal has been received.  Thanks for submitting!');
+        } catch (\Exception $e) {
+            $app->flashNow('error', 'There was an error saving your proposal.  Please verify your information and try again.');
+        }
+
+        $app->render('cfp.html', ['page' => 'cfp']);
+    });
+
     $app->get('/topics', function() use($app) {
         $app->render('topics.html', ['page' => 'topics']);
     })->name('topics');
