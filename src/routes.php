d91d5ade863ea2e43d52d5fa9b6c8e82120e95a1
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-03 06:55:20 -0400

    Remove route names.
    These route names weren't really being used and can complicate things.

--- src/routes.php
+++ src/routes.php
@@ -2,11 +2,11 @@
 return function(\Slim\Slim $app) {
     $app->get('/', function() use($app) {
         $app->render('home.html', ['page' => 'home']);
-    })->name('home');
+    });
 
     $app->get('/cfp', function() use($app) {
         $app->render('cfp.html', ['page' => 'topics']);
-    })->name('cfp');
+    });
 
     $app->post('/cfp', function() use($app) {
         $req = $app->request();
@@ -31,21 +31,21 @@ return function(\Slim\Slim $app) {
 
     $app->get('/topics', function() use($app) {
         $app->render('topics.html', ['page' => 'topics']);
-    })->name('topics');
+    });
 
     $app->get('/people', function() use($app) {
         $app->render('people.html', ['page' => 'people']);
-    })->name('people');
+    });
 
     $app->get('/faq', function() use($app) {
         $app->render('faq.html', ['page' => 'faq']);
-    })->name('faq');
+    });
 
     $app->get('/schedule', function() use($app) {
         $app->render('schedule.html', ['page' => 'schedule']);
-    })->name('schedule');
+    });
 
     $app->get('/survey', function() use($app) {
         $app->render('survey.html', ['page' => 'survey']);
-    })->name('survey');
+    });
 };
