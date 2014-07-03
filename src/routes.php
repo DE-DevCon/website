dc2f6a3ac938a4817b342a5dc200b3570bd4da95
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-03 07:19:03 -0400

    Add strong names to routes and use urlFor to link to them.

--- src/routes.php
+++ src/routes.php
@@ -3,10 +3,10 @@ return function(\Slim\Slim $app) {
     $conference = require 'routes/conference.php';
 
     $app->get('/', function() use($app) {
-        $app->redirect('/2014/');
+        $app->redirect($app->urlFor('2014-home'));
     });
 
     $app->group('/2014', function() use($app, $conference) {
-        $conference($app, ['callForPapers' => true, 'faq' => true]);
+        $conference($app, ['year' => 2014, 'callForPapers' => true, 'faq' => true]);
     });
 };
--- src/routes/conference.php
+++ src/routes/conference.php
@@ -1,15 +1,15 @@
 <?php
 return function(\Slim\Slim $app, array $options) {
-    $app->get('/', function() use($app) {
-        $app->render('home.html', ['page' => 'home']);
-    });
+    $app->get('/', function() use($app, $options) {
+        $app->render('home.html', ['page' => 'home'] + $options);
+    })->name("{$options['year']}-home");
 
     if (isset($options['callForPapers'])) {
-        $app->get('/cfp', function() use($app) {
-            $app->render('cfp.html', ['page' => 'topics']);
-        });
+        $app->get('/cfp', function() use($app, $options) {
+            $app->render('cfp.html', ['page' => 'topics'] + $options);
+        })->name("{$options['year']}-cfp");
 
-        $app->post('/cfp', function() use($app) {
+        $app->post('/cfp', function() use($app, $options) {
             $req = $app->request();
             try {
                 $app->mailgun->sendMessage(
@@ -27,33 +27,33 @@ return function(\Slim\Slim $app, array $options) {
                 $app->flashNow('error', 'There was an error saving your proposal.  Please verify your information and try again.');
             }
 
-            $app->render('cfp.html', ['page' => 'cfp']);
-        });
+            $app->render('cfp.html', ['page' => 'cfp'] + $options);
+        })->name("{$options['year']}-cfp-submit");
     }
 
     if (isset($options['topics'])) {
-        $app->get('/topics', function() use($app) {
-            $app->render('topics.html', ['page' => 'topics']);
-        });
+        $app->get('/topics', function() use($app, $options) {
+            $app->render('topics.html', ['page' => 'topics'] + $options);
+        })->name("{$options['year']}-topics");
 
-        $app->get('/people', function() use($app) {
-            $app->render('people.html', ['page' => 'people']);
-        });
+        $app->get('/people', function() use($app, $options) {
+            $app->render('people.html', ['page' => 'people'] + $options);
+        })->name("{$options['year']}-people");
 
-        $app->get('/schedule', function() use($app) {
-            $app->render('schedule.html', ['page' => 'schedule']);
-        });
+        $app->get('/schedule', function() use($app, $options) {
+            $app->render('schedule.html', ['page' => 'schedule'] + $options);
+        })->name("{$options['year']}-schedule");
     }
 
     if (isset($options['faq'])) {
-        $app->get('/faq', function() use($app) {
-            $app->render('faq.html', ['page' => 'faq']);
-        });
+        $app->get('/faq', function() use($app, $options) {
+            $app->render('faq.html', ['page' => 'faq'] + $options);
+        })->name("{$options['year']}-faq");
     }
 
     if (isset($options['survey'])) {
-        $app->get('/survey', function() use($app) {
-            $app->render('survey.html', ['page' => 'survey']);
-        });
+        $app->get('/survey', function() use($app, $options) {
+            $app->render('survey.html', ['page' => 'survey'] + $options);
+        })->name("{$options['year']}-survey");
     }
 };
