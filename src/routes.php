38db69dd4f3cc5da6ab0be7c4490ea075c5afd84
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-03 07:47:34 -0400

    Add a basic 2013 site.

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,10 @@ return function(\Slim\Slim $app) {
         $app->redirect($app->urlFor('2014-home'));
     });
 
+    $app->group('/2013', function() use($app, $conference) {
+        $conference($app, require 'config/2013.php');
+    });
+
     $app->group('/2014', function() use($app, $conference) {
         $conference($app, require 'config/2014.php');
     });
