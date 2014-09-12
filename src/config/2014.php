e951c5f0f594a8be9ccc3deb7a9577356c474b2c
Chris Ryan <christopher.ryan@dominionenterprises.com>
2014-09-12 12:52:42 -0400

    Added new FAQ question.

--- src/config/2014.php
+++ src/config/2014.php
@@ -13,6 +13,7 @@ return [
         'What is DeDevCon?' => 'DeDevCon is a developer conference being led by Chad Gray and Chris Ryan from floor 12B at Dominion Enterprises. Our goal is to bring the community of sharing you find at a larger developer conference to our co-workers here at Dominion Square.',
         'When is DeDevCon?' => 'DeDevCon is being planned for Friday, December 12th.',
         'Who can attend?' => 'Right now we are looking to have Dominion Enterprise employees attend the conference in the 20th floor conference rooms. We are also working to have the sessions recorded for those who are not in Dominion Square to view.',
+        'Can I give more than one presentation?' => 'If you submit more than one proposal we may select you to give multiple talks.',
         'Will it cost anything?' => 'There is no cost for employees to attend; however, you should get approval with your manager since it may impact your department for the time you are in attendance of the conference.',
         'Will I learn anything?' => 'Perhaps, that is entirely up to you. We are planning to have a broad range of technical topics of varying skill levels that we hope will be useful and educational.',
         'What is the deal with the Narwhal swimming on the bottom of this site?' => 'The Narwhal is our mascot. Lucien Frelin is the awesome designer of the swimming Narwhal.',
