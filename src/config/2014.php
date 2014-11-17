0cb9a5eea44a4587c8670d06422346c3d28897f2
Chris Ryan <christopher.ryan@dominionenterprises.com>
2014-11-17 15:23:43 -0500

    Updated date to match changes.

--- src/config/2014.php
+++ src/config/2014.php
@@ -4,14 +4,14 @@ return [
     'logo' => '/images/DEVcon-blue-small-2014.svg',
     'home' => [
         'news' => [
-            'The second annual DE Dev Con is coming this December 12th! We hope to bring even more great topics to you this year; but, we need your help. If you would like to give a presentation please see our <a href="cfp">Call for Papers</a>.',
+            'The second annual DE Dev Con is coming Monday, December 15th! We have some great topics for you this year. Check back soon to see the schdule of talks.',
             '<a href="https://drive.google.com/folderview?id=0B1RRAO3TxJk2RHBzcDNlcFBYbDA&usp=sharing">View the presentations from last year\'s sessions.</a>',
         ],
     ],
     'callForPapers' => ['due' => '2014-11-06'],
     'faq' => [
         'What is DeDevCon?' => 'DeDevCon is a developer conference being led by Chris Ryan from floor 12B at Dominion Enterprises and supported by TTAG. Our goal is to bring the community of sharing you find at a larger developer conference to our co-workers here at Dominion Square.',
-        'When is DeDevCon?' => 'DeDevCon is being planned for Friday, December 12th.',
+        'When is DeDevCon?' => 'DeDevCon is being planned for Monday, December 15th.',
         'Who can attend?' => 'Right now we are looking to have Dominion Enterprise employees attend the conference in the 20th floor conference rooms. We are also working to have the sessions recorded for those who are not in Dominion Square to view.',
         'Can I give more than one presentation?' => 'If you submit more than one proposal we may select you to give multiple talks.',
         'Will it cost anything?' => 'There is no cost for employees to attend; however, you should get approval with your manager since it may impact your department for the time you are in attendance of the conference.',
