982a0d5b02176b1091785c5a06d684c0d0fbaaf6
Chris Ryan <christopher.ryan@dominionenterprises.com>
2014-12-01 15:56:10 -0500

    Adding People/Talks/Schedule.

--- src/config/2014.php
+++ src/config/2014.php
@@ -11,11 +11,57 @@ return [
     'people' => [
         'anthony_bittle' => [
             'name' => 'Anthony Bittle',
-            'description' => 'Guy with a nose! Ask him about that at the conference.',
+            'description' => 'Anthony has always loved computers and has been writing code since he was 12 years old.  While working for Trader Online team for the last 3 years as a PHP developer, he has contributed to 8 award-winning hackathon projects.  In his spare time he enjoys gaming, programming, and solving Rubik\'s cubes.
+',
         ],
         'brandon_beigay' => [
             'name' => 'Brandon Beigay',
-            'description' => 'Brandon is the project manager for the DWS Recreation team.',
+            'description' => 'Project Manager Recreational Brands Dominion Web Solutions',
+        ],
+        'brandon_shoop' => [
+            'name' => 'Brandon Shoop',
+            'description' => 'Dominion Marine Media',
+        ],
+        'chante_velez' => [
+            'name' => 'Chante Velez',
+            'description' => 'Project Manager Architecture IT Dominion Web Solutions',
+        ],
+        'chris_cataldo' => [
+            'name' => 'Chris Cataldo',
+            'description' => 'Senior Developer Recreational Brands Dominion Web Solutions',
+        ],
+        'guillermo_fisher' => [
+            'name' => 'Guillermo Fisher',
+            'description' => 'Guillermo Fisher joined Trader Electronic Media in November of 2004 as an HTML Developer. Shortly thereafter, he transitioned into a programming role on Trader Publishing Company’s City Web Services team. Guillermo left Trader Publishing just prior to the company’s split and moved on to Harris Connect. Between 2006 and 2009, he took on programming roles at Gannett Media Technologies International, Cox Auto Trader Publishing, and the Nebraska-based Elevate Visual Communications. In November of 2009, he made his way back to Dominion Enterprises as a Senior Programmer with TOL’s Core team. In 2010, Guillermo became part of Dominion Marine Media, working first as a Senior Software Engineer, then Software Architect, and, most recently, Engineering Manager. Earlier this year, he became Director of Enterprise Release Engineering, and has since been managing Dominion Enterprises’ GitHub Enterprise Project and spearheading the OpenDE initiative.<p>
+Guillermo, a Brooklyn native, currently resides in Chesapeake with his wife of 10 years and his four kids; his oldest son is currently attending VCU.',
+        ],
+        'heath_padrick' => [
+            'name' => 'Heath Padrick',
+            'description' => 'Dominion Marine Media',
+        ],
+        'jon_kranz' => [
+            'name' => 'Jon Kranz',
+            'description' => 'Jon Kranz is the TOL Rec group\'s user experience architect and strategist. He and his colleague, Chris Cataldo recently rebuilt their analytics environment from the ground-up. Their shared experience is the impetus for this talk.',
+        ],
+        'jp_murray' => [
+            'name' => 'JP Murray',
+            'description' => 'Dominion Marine Media',
+        ],
+        'kelley_howell' => [
+            'name' => 'Kelley Howell',
+            'description' => 'UI/UX for Homes.com',
+        ],
+        'matt_strapko' => [
+            'name' => 'Matt Strapko',
+            'description' => 'Matt Strapko has been with DE for two and a half years as an HRIS developer of aspect-oriented applications and CORS web services for ASK4HR, DE Portal, and Self-Service.  He will graduate next year with a master\'s in software engineering from East Carolina University',
+        ],
+        'michelle_brooks' => [
+            'name' => 'Michelle Brooks',
+            'description' => 'Senior Project Manager Commercial Brands Dominion Web Solutions',
+        ],
+        'mike_michael' => [
+            'name' => 'Mike Michael',
+            'description' => 'Mike Michael has been working in IT for fifteen years as a Systems Administrator, seven of those years at Dominion Enterprises. He currently works in HRIS supporting their systems and infrastructure and integrating HR systems into single experience. He also manages the Dominion Enterprises\' SSO/SAML environment.',
         ],
         'ryan_goodwin' => [
             'name' => 'Ryan Goodwin',
@@ -28,6 +74,70 @@ return [
             'presenters' => ['ryan_goodwin'],
             'description' => 'Since the NSA files leaked, there has been much change in the information technology world.  In this presentation, we\'ll review what we\'ve learned from the leaks, what steps Google, Yahoo!, and other companies have taken, as well as examine what role we have in this industry-defining upheaval.',
         ],
+        'scrum_jambalaya' => [
+            'title' => 'Lego and Scrum Jambalaya',
+            'presenters' => ['michelle_brooks', 'brandon_beigay', 'chante_velez'],
+            'description' => 'After a brief review attendees will dive right into a simulation game.<ul>
+<li>Simulate Dev Scrum Teams that will design & “release” Lego animals based on requests from “The Business”
+<li>Each animal design will be given a market $ amount that can fluctuate.  Whichever team is the most profitable at the end of the game is the winner!
+<li>Goal is to break down silos, discover ways to work smarter & develop empathy for your team mates.
+</ul>',
+        ],
+        'analytics' => [
+            'title' => 'Architecting Analytics',
+            'presenters' => ['jon_kranz', 'chris_cataldo'],
+            'description' => 'Creating resilient environments for analysis and measurement',
+        ],
+        'employee_experience' => [
+            'title' => 'Integrating the Employee Experience',
+            'presenters' => ['matt_strapko', 'mike_michael'],
+            'description' => 'This will be a presentation of the revamp of the Dominion Enterprises employee portal with IBM WebSphere Portal.  Likely topics to be covered:  project scope and logistics, working with Java EE (JSP, servlets, JSR 286 portlets), data aggregation methods, and socializing the DE employee experience.',
+        ],
+        'vim_ninja' => [
+            'title' => 'How to be a Vim Ninja',
+            'presenters' => ['anthony_bittle'],
+            'description' => 'The Vim editor has one of the steepest learning curves of all editors, but it is also one of the most powerful ways to edit your code.  While Vim\'s features may not be easily discoverable, inside is a world of immense text editing power.  Whether you are new to vim or you\'re already comfortable with its subtleties, come learn how to take your vim skills to the next level.',
+        ],
+        'docker' => [
+            'title' => 'Developing with Docker',
+            'presenters' => ['anthony_bittle'],
+            'description' => 'Docker makes it easy to deploy applications in containers while essentially eliminating dependency problems.  In the last year this has caused quite a revolution in the way we think about dev ops, but how does this affect our development environments?  Come see how to get up and running with Docker as well as how to easily deploy and share a cluster of containers for more complicated development environments.',
+        ],
+        'search_experience' => [
+            'title' => 'Understanding the search experience',
+            'presenters' => ['kelley_howell'],
+            'description' => 'Many of Dominion Enterprise businesses rely on a core product: a product or listing search for consumers. A core mission, for many of us, is to create a search experience that brings buyers and sellers together. We help people find the consumer and durable goods they want to buy - or rent.<p>
+In this presentation, I\'ll draw on a large body of research, as well as my own recent empirical work, studying how people search in general and how they search classified listings more specifically.<p>
+I\'ll summarize the major problems users encounters and the UX design patterns our businesses can use to eliminate pain points and improve the user experience.',
+        ],
+        'apis' => [
+            'title' => 'Building Valuable RESTful APIs',
+            'presenters' => ['guillermo_fisher'],
+            'description' => 'Of the tens of thousands of web services available today, only a distinct few stand out as products that are attractive to developers and valuable to businesses. In this talk, we\'ll explore ways to build value into our RESTful APIs, and take a deeper look at the prominent practices and strategies being employed in developing, documenting, and ultimately monetizing them.',
+        ],
+        'functional' => [
+            'title' => 'Functional Programming',
+            'presenters' => ['brandon_shoop', 'heath_padrick'],
+            'description' => 'A brief overview and explain what \'functional\' means, then spend some time diving into and dissecting code examples. The code examples would probably be in Scala, JS, and a little PHP.',
+        ],
+        'agile' => [
+            'title' => 'Agile Programming: One Dev\'s Perspective',
+            'presenters' => ['jp_murray'],
+            'description' => 'Agile Programming from the perspective of a developer who has put the principles in practice.',
+        ],
+        'TBD' => [
+            'title' => 'To Be Determined',
+            'description' => 'We are still working on the details for this presentation.',
+        ]
+    ],
+    'schedule' => [
+        '2014-12-15 09:00 EST' => ['room1' => 'vim_ninja', 'room2' => 'data_stewards'],
+        '2014-12-15 10:00 EST' => ['room1' => 'functional', 'room2' => 'analytics'],
+        '2014-12-15 11:00 EST' => ['room1' => 'docker', 'room2' => 'scrum_jambalaya'],
+        '2014-12-15 12:00 EST' => 'Lunch',
+        '2014-12-15 13:00 EST' => ['room1' => 'apis', 'room2' => 'employee_experience'],
+        '2014-12-15 14:00 EST' => ['room1' => 'TBD', 'room2' => 'search_experience'],
+        '2014-12-15 15:00 EST' => ['room1' => 'agile', 'room2' => 'TBD'],
     ],
     'faq' => [
         'What is DeDevCon?' => 'DeDevCon is a developer conference being led by Chris Ryan from floor 12B at Dominion Enterprises and supported by TTAG. Our goal is to bring the community of sharing you find at a larger developer conference to our co-workers here at Dominion Square.',
