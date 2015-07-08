0b1cee16a70527fe7899b17eee08cfe608e9384b
chadicus <chadwickgray@gmail.com>
2015-07-08 10:46:14 -0400

    Add 2015 config/routes

---
+++ src/config/2015.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'year' => 2015,
+    'logo' => '/images/DEVcon-blue-small-no-date.svg',
+    'home' => [
+        'news' => [
+            'DE DevCon III',
+            'Coming This October',
+        ],
+    ],
+    'callForPapers' => ['due' => '2015-09-04'],
+    'people' => [],
+    'schedule' => [],
+    'topics' => [],
+    'faq' => [
+        'What is DeDevCon?' => 'DeDevCon is a developer conference being led by Chad Gray and Chris Ryan from floor 12B at Dominion Enterprises. Our goal is to bring the community of sharing you find at a larger developer conference to our co-workers here at Dominion Square.',
+        'When is DeDevCon?' => 'DeDevCon is being planned for early December 2014. We will update the site with more information when it is available.',
+        'Who can attend?' => 'Right now we are looking to have Dominion Enterprise employees attend the conference in the 20th floor conference rooms. We are also working to have the sessions recorded for those who are not in Dominion Square to view.',
+        'Will it cost anything?' => 'There is no cost for employees to attend; however, you should get approval with your manager since it may impact your department for the time you are in attendance of the conference.',
+        'Will I learn anything?' => 'Perhaps, that is entirely up to you. We are planning to have a broad range of technical topics of varying skill levels that we hope will be useful and educational.',
+        'What is the deal with the Narwhal swimming on the bottom of this site?' => 'The Narwhal is our mascot. Lucien Frelin is the awesome designer of the swimming Narwhal.',
+        'Does the Narwhal have a name?' => 'The Narwhal\'s name is Norman T Narwhal.',
+    ],
+];
--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@ return function(\Slim\Slim $app) {
     $conference = require 'routes/conference.php';
 
     $app->get('/', function() use($app) {
-        $app->redirect($app->urlFor('2014-home'));
+        $app->redirect($app->urlFor('2015-home'));
     });
 
     $app->group('/2013', function() use($app, $conference) {
@@ -12,5 +12,9 @@ return function(\Slim\Slim $app) {
 
     $app->group('/2014', function() use($app, $conference) {
         $conference($app, require 'config/2014.php');
+    });
+
+    $app->group('/2015', function() use($app, $conference) {
+        $conference($app, require 'config/2015.php');
     });
 };
