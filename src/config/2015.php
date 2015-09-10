627add38ec2250c1a889ff41a8f09bf10fa9d368
Chad Gray <chadwickgray@gmail.com>
2015-09-10 16:24:48 -0400

    Add 2015 people, topics, and schedule

--- src/config/2015.php
+++ src/config/2015.php
@@ -4,12 +4,247 @@ return [
     'logo' => 'http://cdn0.traderonline.com/v1/media/de-dev-con-new-blue.png',
     'home' => [
         'news' => [
-            'The third annual DE Dev Con is coming Thursday, October 8th! We have some great topics for you this year. The full <a href="/2015/schedule">Schedule</a> is online now!',
+            'DE DevCon III is Thursday, October 8th! The full <a href="/2015/schedule">schedule</a> is online now!',
         ],
     ],
-    'people' => [],
-    'schedule' => [],
-    'topics' => [],
+    'people' => [
+        'marius_de_beer' => [
+            'name' => 'Marius de Beer',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'jeff_wolski' => [
+            'name' => 'Jeff Wolski',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'josiah_baker' => [
+            'name' => 'Josiah Baker',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'alena_devan' => [
+            'name' => 'Alena Devan',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'kelley_howell' => [
+            'name' => 'Kelley Howell',
+            'description' => 'Kelly Howell is UX Research Manager at Homes.com. She has been working in the field of User Experience and User Interface development since 1996 when she helped build one of the first online learning courses at her university. In the process, she was introduced to the wild, wild world of Web development, user experience research, and interaction desgin.',
+            'twitter' => null,
+        ],
+        'lucien_frelin' => [
+            'name' => 'Lucien Frelin',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'randolf_julian' => [
+            'name' => 'Randolf Julian',
+            'description' => "I work at Dominion Web Solutions (DWS) as a Senior Database Administrator. I've been with the company 14 years.",
+            'twitter' => null,
+        ],
+        'brandon_beigay' => [
+            'name' => 'Brandon Beigay',
+            'description' => 'Project Manager Recreational Brands Dominion Web Solutions',
+            'twitter' => null,
+        ],
+        'michelle_brooks' => [
+            'name' => 'Michelle Brooks',
+            'description' => 'Senior Project Manager Commercial Brands Dominion Web Solutions',
+            'twitter' => null,
+        ],
+        'chante_velez' => [
+            'name' => 'Chante Velez',
+            'description' => 'Project Manager Architecture IT Dominion Web Solutions',
+            'twitter' => null,
+        ],
+        'chris_ryan' => [
+            'name' => 'Chris Ryan',
+            'description' => "Lead Developer Recreational Brands Dominion Web Solutions. With over 15 years of software development experience Chris brings an experienced knowledge of problem solving in the software field. With a passion for finding solutions, creating new things, and a slow, deliberate pace to understanding problems. Chris originally joined Trader Publishing's Electronic Media group in the late 90's. After pursuing other opportunities he then returned back to Dominion in the Trader Online group back in 2011.",
+            'twitter' => null,
+        ],
+        'guillermo_fisher' => [
+            'name' => 'Guillermo Fisher',
+            'description' => 'Guillermo Fisher joined Trader Electronic Media in November of 2004 as an HTML Developer. Shortly thereafter, he transitioned into a programming role on Trader Publishing Company’s City Web Services team. Guillermo left Trader Publishing just prior to the company’s split and moved on to Harris Connect. Between 2006 and 2009, he took on programming roles at Gannett Media Technologies International, Cox Auto Trader Publishing, and the Nebraska-based Elevate Visual Communications. In November of 2009, he made his way back to Dominion Enterprises as a Senior Programmer with TOL’s Core team. In 2010, Guillermo became part of Dominion Marine Media, working first as a Senior Software Engineer, then Software Architect, and, most recently, Engineering Manager. Earlier this year, he became Director of Enterprise Release Engineering, and has since been managing Dominion Enterprises’ GitHub Enterprise Project and spearheading the OpenDE initiative.<p>
+Guillermo, a Brooklyn native, currently resides in Chesapeake with his wife of 10 years and his four kids; his oldest son is currently attending VCU.',
+            'twitter' => null,
+        ],
+        'joe_legeckis' => [
+            'name' => 'Joe Legeckis',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'robert_bittle' => [
+            'name' => 'Robert Bittle',
+            'description' => "Anthony has always loved computers and has been writing code since he was 12 years old.  While working for Trader Online team for the last 4 years as a PHP developer, he has contributed to 8 award-winning hackathon projects.  In his spare time he enjoys gaming, programming, and solving Rubik's cubes.",
+            'twitter' => 'guywithnose',
+        ],
+        'algimantas_krasauskas' => [
+            'name' => 'Algimantas Krasauskas',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'heath_padrick' => [
+            'name' => 'Heath Padrick',
+            'description' => 'Using FrontPage 2000 and AngelFire, Heath started his career in web design by fulfilling the requirement for a website that his band needed at the time. From there, a passion for web development consumed him.<p>Heath joined Dominion Marine Media in March of 2012 and quickly fell in love with the atmosphere and personalities of his co-workers.<p> He lives in Yorktown, VA with his wife, son, and brand new daughter. Most weekends are filled by spending time with family, practicing guitar, and fighting with the entertainment system.',
+            'twitter' => null,
+        ],
+        'steve_jones' => [
+            'name' => 'Steve Jones',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'alfredo_liu_perez' => [
+            'name' => 'Alfredo Liu-Perez',
+            'description' => null,
+            'twitter' => null,
+        ],
+        'jose_mateo' => [
+            'name' => 'Jose Mateo',
+            'description' => null,
+            'twitter' => null,
+        ],
+    ],
+    'schedule' => [
+        '2013-12-13 08:30' => 'Coffee/Opening remarks',
+        '2015-10-08 09:00' => '<h4><a href="/2015/topics#preventing_burnout">Preventing Burnout</a></h4><h5><a href="/2015/people#marius_de_beer">Marius de Beer</a></h5>',
+        '2015-10-08 10:00' => [
+            'room1' => 'fine_tuning_sql_statements',
+            'room2' => 'information_radiators',
+            'room3' => 'side_effects_in_languages',
+        ],
+        '2015-10-08 11:00' => [
+            'room1' => 'legoman',
+            'room2' => 'ad_features_analysis_using_multivariable_regression',
+            'room3' => 'getting_reactive_with_meteor_js',
+        ],
+        '2015-10-08 12:00' => 'Break',
+        '2015-10-08 13:00' => [
+            'room1' => 'refactoring',
+            'room2' => 'visualizing_data',
+            'room3' => 'event_streaming',
+        ],
+        '2015-10-08 14:00' => [
+            'room1' => 'cli_applications',
+            'room2' => 'what_is_data_science',
+            'room3' => 'dynamic_javascript_content',
+        ],
+        '2015-10-08 15:00' => [
+            'room1' => 'automated_docker',
+            'room2' => 'making_bad_ass_users_',
+            'room3' => 'getting_started_with_android_development',
+        ],
+        '2015-10-08 16:00' => [
+            'room1' => 'getting_started_with_chef',
+            'room2' => 'good_design_is_good_for_business_',
+            'room3' => null,
+        ],
+    ],
+    'topics' => [
+        'preventing_burnout' => [
+            'title' => 'Preventing Burnout',
+            'presenters' => ['marius_de_beer'],
+            'description' => ' Burnout has been synonymous with the IT industry since its birth in the 60s and many good people have suffered the consequences.  Everything in our industry is geared to pushing us towards burnout, with those resisting it often labeled as lazy or “not pulling their weight.” Even tinkering / playing / learning new technologies in your own time can be detrimental.  The objective of this talk is to equip people with a number of practical techniques to prevent burnout and attain that elusive “sustainable pace,” regardless of your boss’ demands.',
+        ],
+        'information_radiators' => [
+            'title' => 'Information Radiators',
+            'presenters' => ['jeff_wolski'],
+            'description' => ' I\'d like to talk about information radiators and dashboards here at DE. I developed one for ForRent which is being widely used, and it has helped our business monitor how changes in our code affect our users.',
+        ],
+        'ad_features_analysis_using_multivariable_regression' => [
+            'title' => 'Ad Features Analysis using Multivariable Regression',
+            'presenters' => ['josiah_baker'],
+            'description' => ' A pretty in depth description of how historic ad activity can be used to create a regression model which would then be able to answer questions like "How do photos increase my clicks/leads compared to ads without photos," or "What is the impact of making my ad a featured or premium ad."  Details on how the model is built and then how it works to predict clicks and leads on new ads.',
+        ],
+        'visualizing_data' => [
+            'title' => 'Visualizing Data',
+            'presenters' => ['alena_devan'],
+            'description' => ' The growing amount of data being collected and analyzed requires innovative data platform solutions. In this talk I will review some of the available technologies to store, manipulate, and visualize data.',
+        ],
+        'what_is_data_science' => [
+            'title' => 'What is Data Science',
+            'presenters' => ['josiah_baker'],
+            'description' => ' A high level discussion of what Data Science is and how it applies to the Web Advertising industry.  Would include brief history, notable examples in other industries, and examples of how it is being used today here at Dominion Enterprises.',
+        ],
+        'making_bad_ass_users_' => [
+            'title' => 'Making Bad@$$ Users.',
+            'presenters' => ['kelley_howell'],
+            'description' => ' This talk will show IT and product teams how to design their applications for mastery. Everyone talk about designing for emotion or designing for delight. But those are concepts that are either too broad or too superficial. What we really want to do is design for mastery - or, turn our users into Bad Ass Users.',
+        ],
+        'good_design_is_good_for_business_' => [
+            'title' => 'Good Design is Good for Business.',
+            'presenters' => ['lucien_frelin'],
+            'description' => ' Design is the not only the polish on a graphic. It is quickly becoming the factor that leads innovation.  Design in so many industries today is the defining advantage and concurrently a tool used to fend off competitors and create a owned differentiation.',
+        ],
+        'fine_tuning_sql_statements' =>
+        [
+            'title' => 'SQL Statement: Basic Fine-tuning',
+            'presenters' => ['randolf_julian'],
+            'description' => 'Data are retrieved from the database using SQL statements. The same result set can be achieved even if queries are written differently. However, the best one should be used when performance is considered. The talk is about basic fine-tuning tips; ones that can help out in making SQL statements run efficiently. A demo will also be given using an Oracle database.',
+        ],
+        'legoman' => [
+            'title' => 'LegoMan',
+            'presenters' => ['brandon_beigay', 'michelle_brooks', 'chante_velez'],
+            'description' => '<p>Fun hands-on Lego building game (for on-site participants only) that will:</p>'
+                           . '<ul><li>Encourage Team Building</li><li>Encourage Communication</li> <li>Encourage Leadership Skills</li>'
+                           . '<li>Encourage Iterative Development</li><li>Encourage Empathy for Product Developers or anyone who has to capture User Requirements/Stories</li></ul>',
+        ],
+        'refactoring' => [
+            'title' => 'Refactoring: Updating Code Sanely',
+            'presenters' => ['chris_ryan'],
+            'description' => 'Refactoring is one of the most important skills for a developer to have. We will discuss what refactoring is and rules to help make it easier. Some real examples will be reviewed along with thoughts about why the code should be factored with the results. While challenging refactoring code can be fun and improve code significantly.',
+        ],
+        'cli_applications' => [
+            'title' => 'CLI Applications',
+            'presenters' => ['guillermo_fisher'],
+            'description' => ' You should write tests for your portable, well-written CLI applications too, right? Right! I\'ll talk about PHP CLI apps.',
+        ],
+        'automated_docker' => [
+            'title' => 'Automated Docker',
+            'presenters' => ['joe_legeckis'],
+            'description' => ' Automated Docker container building, testing, and deployment in large scale production environments.',
+        ],
+        'getting_started_with_chef' => [
+            'title' => 'Getting started with Chef',
+            'presenters' => ['robert_bittle'],
+            'description' => ' Chef makes it easy to manage a large number of servers without having a lot of manual actions.  From code deployment to software updates, Chef takes the pain out of scaling up your infrastructure.  In this talk we\'ll cover the basics of getting Chef installed as well where to find and how to set up all the  components you\'ll need to get going.',
+        ],
+        'side_effects_in_languages' => [
+            'title' => 'Side-effects in Languages',
+            'presenters' => ['algimantas_krasauskas'],
+            'description' => ' I would love to tack of side-effects in languages like Javascript, when is it OK to use them when not.  My talk would mainly focus, the habit\'s while working with systems like Javascript, suggested patterns and focus on immutability.',
+        ],
+        'getting_reactive_with_meteor_js' => [
+            'title' => 'Getting reactive with Meteor JS',
+            'presenters' => ['heath_padrick'],
+            'description' => '<p>I would like to share my excitement and energy for the Meteor JS framework. In doing so, I hope to encourage others to become familiar with Reactive Programming, and introduce them to the awesome community Meteor has created.</p><p>Topics covered</p><ul>
+            <li>What is Reactive Programing</li>
+            <li>What is Meteor JS</li>
+            <li>Creating a Meteor JS project</li>
+            <li>Creating a Meteor package</li>
+            <li>Testing with Velocity</li>
+            <li>Identifying different SUTs and how to test them</li>
+            <li>Overview of the Meteor community</li>
+            <li>Show off some exciting packages</li>
+            </ul>
+            <p>Most of the "creating" topics will be covered by live coding examples. We will build some sort of application.</p>',
+        ],
+        'event_streaming' => [
+            'title' => 'Event Streaming',
+            'presenters' => ['steve_jones'],
+            'description' => ' Near real-time event streaming at ForRent',
+        ],
+        'dynamic_javascript_content' => [
+            'title' => 'Dynamic Javascript Content',
+            'presenters' => ['alfredo_liu_perez'],
+            'description' => ' How to use Python, BeautifulSoup, and PhantomJS to scrape websites that display dynamic javascript content.',
+        ],
+        'getting_started_with_android_development' => [
+            'title' => 'Getting started with Android Development',
+            'presenters' => ['jose_mateo'],
+            'description' => ' Demonstrate how to quickly build android apps using open source libraries. The talk would cover how to set up an android project, consume RESTful services, and display data using android standard components.',
+        ],
+    ],
     'faq' => [
         'What is DE DevCon?' => 'DE DevCon is a developer conference being led by Chad Gray and Chris Ryan from floor 12B at Dominion Enterprises. Our goal is to bring the community of sharing you find at a larger developer conference to our co-workers here at Dominion Square.',
         'When is DE DevCon?' => 'DE DevCon III will be held on Thursday October 8th, 2015.',
