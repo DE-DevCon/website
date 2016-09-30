fadbb3155ef7bb3580647c64eaa978b3a3cd2514
Chad Gray <chadwickgray@gmail.com>
2016-09-30 12:40:50 -0400

    fix title

--- src/config/2016.php
+++ src/config/2016.php
@@ -295,7 +295,7 @@ Guillermo, a Brooklyn native, currently resides in Chesapeake with his wife of 1
             'description' => 'I plan to cover the meaning of true user experience and purposeful product creation.  I expect the attendees to begin to look at software development from the users perspective.  I will show techniques that will help attendees understand the need for thoughtful development. I will help attendees appreciate success in the form of fulfilling product needs. I will provide examples of successful user experience from large and small companies inside and outside of tech. I will show and warn of the hazards of not creating thoughful experiences.',
         ],
         '57c5e408e9e64' => [
-            'title' => 'Data Inventory Explore',
+            'title' => 'Data Inventory Explorer',
             'presenters' => [ 'jeff_wolski', 'adam_vandover' ],
             'description' => 'The Data Inventory Explorer is an initiative by the ForRent research and development team which harvests, consolidates, stores, and displays metadata. Users can explore the relationships among data sources across a variety of database platforms.',
         ],
